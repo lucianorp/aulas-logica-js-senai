1e5ccf72025d816953acc22ac3662cf31ddbb32a
Professor SESI/SENAI <Professor@SN-371163.edu.fiesc.com.br>
2025-04-22 22:25:39 -0300

    feat:tipos de variaveis e exercicio 01

---
+++ exercicio01.js
@@ -0,0 +1,7 @@
+// Crie uma variável que seja um array de objeto que tenha os seguintes pares: chave e valor
+
+// nome da variável => users
+
+// valores => um objeto com os valores login, password, cursos, sendo cursos um array com dois valores
+
+// renderizar no console a seguinte frase com o primeiro user do array 'Meu login é ${login}, minha senha é ${password} e faço os cursos x e y'
--- variaveis.js
+++ variaveis.js
@@ -12,9 +12,48 @@ var nome = "João"*/
 
 //variável declarada com const
 
-const nome = "Luciano"
+const nome = "Luciano" //string
 
-nome = "João"
+const sobrenome = "Rocha"
+
+const valor = 21.90 // number
+
+const isLogin = false // boolean
+
+let preco //undefined
+
+const endereco = null //null
+
+const alunos = ["João","Maria","José"] //array
 
 
-console.log(nome)
+const alunoDoSenai ={
+    nome:"Luciano",
+    sobrenome:"Rocha",
+    turno:"noturno",
+    idade:42,
+    cursos:["games","programação web"]
+}
+
+const alunosDoSenai = [
+    {
+        login:"joao",
+        email:"joao@gmail.com",
+        cursos:["games","web"]
+    },
+    {
+        login:"maria",
+        email:"maria@gmail.com",
+        cursos:["design","games"]
+    }
+]
+
+// console.log(nome + " " + sobrenome)
+
+console.log(`Meu nome completo é ${nome} ${sobrenome}`)
+
+
+// console.log(alunosDoSenai[1].cursos[0])
+
+
+//console.log(alunos[0]) //pega o valor do índice do array
