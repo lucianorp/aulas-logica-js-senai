b176cdc65338815babfd8b187d2621af938ec1d8
Professor SESI/SENAI <Professor@SN-371163.edu.fiesc.com.br>
2025-04-23 22:22:06 -0300

    feat: operadores em js

--- exercicio01.js
+++ exercicio01.js
@@ -5,3 +5,30 @@
 // valores => um objeto com os valores login, password, cursos, sendo cursos um array com dois valores
 
 // renderizar no console a seguinte frase com o primeiro user do array 'Meu login é ${login}, minha senha é ${password} e faço os cursos x e y'
+
+const product = {
+    nome:"produto x",
+    valor:50.00,
+    quantidade:3
+}
+
+const product2 = {
+    nome:"produto y",
+    valor:50.00,
+    quantidade:3
+}
+
+console.log(`${product.valor + product2.valor}`)
+
+const login = false
+
+const isLogged = login? "logado" : "não logado"
+
+console.log(`${isLogged}`)
+
+
+valor01 = 51
+
+valor = 2
+console.log(`${valor ** 3}`)
+console.log(`${valor01%2 === 0}`)
---
+++ operadores-logicos.js
@@ -0,0 +1,19 @@
+// const valor01 = 50
+// const valor02 = 20
+
+// && => Operador de conjunção
+// || => Operador de disjunção
+// ! => negação 
+
+// console.log(`${valor01>20 || valor02>=30}`)
+
+const temCarteira = true
+const idade = 15
+
+const resultadoDoTeste = idade>=18 && temCarteira
+
+console.log(`${resultadoDoTeste ? "pode dirigir" : "Não pode dirigir"}`)
+
+// faça um algoritmo para verificar se o candidato pode dirigir. Para isso, ele precisa 
+// 1 - ter idade >= 18
+// 2 - ter carteira 
